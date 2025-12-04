diffusion calc is correct, need to scale units

method.py
@@ -6,6 +6,177 @@ import scipy.sparse as sparse
 import copy
 
 
+def global_mat_elementwise(mesh : tools.Discretization, sigma, D):
+
+    nx = mesh.nx + 0
+    dx = mesh.dx + 0
+
+    global_matrix = sparse.lil_array((4*nx, 4*nx))
+
+    half = 2*nx
+    
+
+    mll = 2
+    mlr = 1
+    mrl = 1
+    mrr = 2
+
+    # print(f"D = {D[0]}")
+
+
+    for i in range(0, nx-1):
+        # left cells. Right side conditions
+        I_next_R = 2*i - 1
+        I_cell_L = 2*i
+        I_cell_R = 2*i +1
+        I_next_L = 2*i +2
+
+        F_next_R = I_next_R + half
+        F_cell_L = I_cell_L + half
+        F_cell_R = I_cell_R + half
+        F_next_L = I_next_L + half
+
+        # left_row_z   = 2*i
+        right_row_z = 2*i +1
+
+        # left_row_f = left_row_z + half
+        right_row_f = right_row_z + half
+
+        # print(f"cell number {i}, right equations-- rows{right_row_z}, {right_row_f}")
+
+        # right equation
+            # Zeroth moment
+        global_matrix[right_row_z, F_next_L] = 1/2
+        global_matrix[right_row_z, F_cell_L] = -1/2
+        global_matrix[right_row_z, I_cell_R] = 1/4
+        global_matrix[right_row_z, I_next_L] = -1/4
+        global_matrix[right_row_z, I_cell_L] = sigma[i]*mrl*dx/6
+        global_matrix[right_row_z, I_cell_R] += sigma[i]*mrr*dx/6
+            # first moment
+        global_matrix[right_row_f, F_cell_L] = dx*mrl/(6*D[i])
+        global_matrix[right_row_f, F_cell_R] = dx*mrr/(6*D[i])
+        global_matrix[right_row_f, F_cell_R] += 3/4
+        global_matrix[right_row_f, F_next_L] = -3/4
+        global_matrix[right_row_f, I_next_L] = 1/2
+        global_matrix[right_row_f, I_cell_L] = -1/2
+
+
+
+    for i in range(1, nx):
+        # right cells. Left side conditions
+
+        
+        I_next_R = 2*i - 1
+        I_cell_L = 2*i
+        I_cell_R = 2*i +1
+        I_next_L = 2*i +2
+
+        F_next_R = I_next_R + half
+        F_cell_L = I_cell_L + half
+        F_cell_R = I_cell_R + half
+        F_next_L = I_next_L + half
+
+        left_row_z   = 2*i
+        # right_row_z = 2*i +1
+
+        left_row_f = left_row_z + half
+        # right_row_f = right_row_z + half
+
+
+        # print(f"cell number {i}, left equations. rows {left_row_f}, {left_row_z}")
+
+        # left equation
+
+        # Zeroth moment
+        global_matrix[left_row_z, F_cell_R] = 1/2
+        global_matrix[left_row_z, F_next_R] = -1/2
+        global_matrix[left_row_z, I_next_R] = -1/4
+        global_matrix[left_row_z, I_cell_L] = 1/4
+        global_matrix[left_row_z, I_cell_L] += sigma[i]*mll*dx/6
+        global_matrix[left_row_z, I_cell_R] += sigma[i]*mlr*dx/6
+
+        # first moment
+        global_matrix[left_row_f, F_cell_L] = dx*mll/(6*D[i])
+        global_matrix[left_row_f, F_cell_R] = dx*mlr/(6*D[i])
+        global_matrix[left_row_f, F_next_R] = -3/4
+        global_matrix[left_row_f, F_cell_L] += 3/4
+        global_matrix[left_row_f, I_cell_R] = 1/2
+        global_matrix[left_row_f, I_next_R] = -1/2
+
+
+    # left boundary cell, left equation
+    i = 0
+    # I_next_R = 2*i - 1
+    I_cell_L = 2*i
+    I_cell_R = 2*i +1
+    I_next_L = 2*i +2
+    # F_next_R = I_next_R + half
+    F_cell_L = I_cell_L + half
+    F_cell_R = I_cell_R + half
+    F_next_L = I_next_L + half
+    left_row_z   = 2*i
+    # right_row_z = 2*i +1
+    left_row_f = left_row_z + half
+
+    # Zeroth moment
+    global_matrix[left_row_z, F_cell_R] = 1/2
+    # global_matrix[left_row_z, F_next_R] = -1/2
+    # global_matrix[left_row_z, I_next_R] = -1/4
+    global_matrix[left_row_z, I_cell_L] = 1/4
+    global_matrix[left_row_z, I_cell_L] += sigma[i]*mll*dx/6
+    global_matrix[left_row_z, I_cell_R] = sigma[i]*mlr*dx/6
+
+    # first moment
+    global_matrix[left_row_f, F_cell_L] = dx*mll/(6*D[i])
+    global_matrix[left_row_f, F_cell_R] = dx*mlr/(6*D[i])
+    # global_matrix[left_row_f, F_next_R] = -D[i]*3/4
+    global_matrix[left_row_f, F_cell_L] += 3/4
+    global_matrix[left_row_f, I_cell_R] = 1/2
+    # global_matrix[left_row_f, I_next_R] = -D[i]*1/2
+
+
+    # right boundary, right equation
+    i = nx-1
+    I_next_R = 2*i - 1
+    I_cell_L = 2*i
+    I_cell_R = 2*i +1
+    # I_next_L = 2*i +2
+    F_next_R = I_next_R + half
+    F_cell_L = I_cell_L + half
+    F_cell_R = I_cell_R + half
+    # F_next_L = I_next_L + half
+    # left_row_z   = 2*i
+    right_row_z = 2*i +1
+    # left_row_f = left_row_z + half
+    right_row_f = right_row_z + half
+
+    # right equation
+    # Zeroth moment
+    # global_matrix[right_row_z, F_next_L] = 1/2
+    global_matrix[right_row_z, F_cell_L] = -1/2
+    global_matrix[right_row_z, I_cell_R] = 1/4
+    # global_matrix[right_row_z, I_next_L] = -1/4
+    global_matrix[right_row_z, I_cell_L] = sigma[i]*mrl*dx/6
+    global_matrix[right_row_z, I_cell_R] += sigma[i]*mrr*dx/6
+    # first moment
+    global_matrix[right_row_f, F_cell_L] = dx*mrl/(6*D[i])
+    global_matrix[right_row_f, F_cell_R] = dx*mrr/(6*D[i])
+    global_matrix[right_row_f, F_cell_R] += 3/4
+    # global_matrix[right_row_f, F_next_L] = -D[i]*3/4
+    # global_matrix[right_row_f, I_next_L] = D[i]*1/2
+    global_matrix[right_row_f, I_cell_L] = -1/2
+
+
+    global_matrix = global_matrix.tocsr()
+
+    # print("Global Matrix assembled.")
+    # print(f"Highest abs: {numpy.max(numpy.abs(global_matrix.todense()))}")
+    # print(f"Lowest abs (nonzero): {numpy.min(numpy.abs(global_matrix.data))}")
+    return global_matrix
+
+
+
+
 def assemble_global_matrix(mesh  : tools.Discretization, sigma, D):
     # LHS of zeroth moment equation plus Fick's Law.
     # works in the general case-- high order or low order.
@@ -17,8 +188,6 @@ def assemble_global_matrix(mesh  : tools.Discretization, sigma, D):
     dx = mesh.dx + 0
     
     global_matrix = sparse.lil_array((4*nx, 4*nx))
-    global_source = sparse.lil_array((4*nx, 1))
-
     # interior elements
     b_0i = tools.b_0i
     b_0f = tools.b_0f
@@ -27,9 +196,6 @@ def assemble_global_matrix(mesh  : tools.Discretization, sigma, D):
     a  = tools.a_f 
 
     M_wide = tools.M_wide
-
-    local_operator = numpy.zeros((4, 4))
-    intensity_op = numpy.zeros((4, 4))
 
     # interior elements
     for i in range(1, nx-1):
@@ -90,9 +256,6 @@ def get_HO_source(
     nx = mesh.nx
     M = tools.M
 
-
-
-
     mass_global = sparse.lil_array((2*nx, 2*nx))
 
     for i in range(0, nx):
@@ -101,20 +264,64 @@ def get_HO_source(
 
     fiss = numpy.zeros((mesh.ng, 2*nx))
 
+    # print(k)
+    # print("k")
+
+
+    # print(coeff.S[k])
+    # print("S")
+
+    # print(coeff.q[k])
+    # print("q")
+
+
+    # print(coeff.db_dt[k])
+    # print("dbdt")
+    # print(numpy.sum(coeff.kappa * coeff.db_dt, axis=0))
+    # print("kappa * dbdt sum")
+
+    # print(f"Cv must be : {((1/coeff.eta[0]) -1) * numpy.sum(coeff.kappa * coeff.db_dt, axis=0) * mesh.dt }")
+
+
+    # print(coeff.eta[0])
+    # print("eta")
+    # print(coeff.chi[k])
+    # print("chi")
+
+    # print(numpy.sum(coeff.chi, axis=0))
+    # print("sum chi")
+    # print(f"max chi =  {numpy.max(coeff.chi)}")
+
+    # print(coeff.chi[:, 0])
+    # print("chi spectrum")
+
+    # print(coeff.q[k])
+    # print("q")
+
+
+    # print(coeff.kappa[k]*coeff.beta[k] + (coeff.eta * coeff.chi[k] *-numpy.sum(coeff.kappa * coeff.beta, axis=0)))
+    # print("q should be this")
+
     for g in range(0, mesh.ng):
         fiss[g] += tools.dbl(coeff.sig_f[g])*(prev_I[g])
 
         
     source[0:2*mesh.nx] = (dx*( mass_global @ coeff.S[k])
                 + (tools.dbl(coeff.eta) * tools.dbl(coeff.chi[k])
-                * dx * mass_global @ numpy.sum(fiss, axis=0)))
+                * dx* (mass_global @ numpy.sum(fiss, axis=0))))
     # add BCs
     source[0] += mesh.F_BC[k, 0]
-    source[(2*mesh.nx)-1] += mesh.F_BC[k, 1]
+    source[(2*mesh.nx)-1] += -mesh.F_BC[k, 1]
 
-    source[2*mesh.nx] += coeff.D[k, 0]*mesh.I_BC[k, 0]
-    source[-1] +=        -coeff.D[k, -1]*mesh.I_BC[k, 1]
+    # print(f"D dimensions : {coeff.D.shape}")
 
+    # source[2*mesh.nx] += coeff.D[k, 0]*mesh.I_BC[k, 0]
+    # source[-1] +=        -coeff.D[k, -1]*mesh.I_BC[k, 1]
+
+    source[2*mesh.nx] += mesh.I_BC[k, 0]
+    source[-1] +=        -mesh.I_BC[k, 1]
+
+    # print("D IS SET TO ZERO FOR TESTING IN SOURCE")
 
 
 
@@ -127,8 +334,31 @@ def get_LO_source():
 
 def high_order_assembly(mesh : tools.Discretization, coeff : tools.MG_coefficients, last_iter_I, k):
     system = tools.Global_system()
-    system.mat = assemble_global_matrix(mesh, coeff.sig_a+coeff.sig_f[k], coeff.D[k])
+    # system.mat = assemble_global_matrix(mesh, coeff.sig_a+coeff.sig_f[k], coeff.D[k])
+
+    # print(coeff.sig_a+coeff.sig_f[k])
+    # print("sigma in HO assembly")
+
+    system.mat = global_mat_elementwise(mesh, coeff.sig_a+coeff.sig_f[k], coeff.D[k])
+    # print(f"D IS SET TO ZERO FOR TESTING")
     system.src = get_HO_source(mesh, last_iter_I, coeff, k)
+
+    # print(system.mat.todense()[0:6,:])
+    # print("Global matrix snippet")
+
+    # print(system.src[:])
+    # print("source")
+
+    print(coeff.D[k])
+    print("D")
+
+    print(coeff.sig_a)
+    print("sigma_a")
+    
+    print(coeff.sig_f[k])
+    print("sigma_f")
+
+
     return system
 
 def get_grey_constants():
@@ -165,46 +395,45 @@ def unaccelerated_loop(mesh : tools.Discretization,
     I_prev = sol_prev.intensity[:]
     coeff.assign(mesh, kappa, sol_prev, T_prev, Cv, Q)
 
-    print(coeff.S[0])
-    print("Reemission source")
+    # print(coeff.S[0])
+    # print("Reemission source")
 
-    print(coeff.q[0])
-    print("q source")
+    # print(coeff.q[0])
+    # print("q source")
     
-
-    
-
-
     updated_solution = numpy.zeros((mesh.ng, 4*mesh.nx))
     last_iteration = numpy.zeros((mesh.ng, 4*mesh.nx))
     last_iteration[:,:] = sol_prev.vec[:].copy()
     
-
-
     change = [1]
     iter = 0
 
-    while (numpy.max(change) > mesh.eps) and (iter < 4) :
+    while (numpy.max(change) > mesh.eps) :
+    # while (iter < 20) :
         iter += 1
         print(f"Iteration {iter}, change = {numpy.max(change)}")
 
+        print("spectral radius:")
+        print(coeff.eta * numpy.sum((coeff.chi * coeff.sig_f)/(coeff.sig_a + coeff.sig_f), axis=0))
+
+        print("eta:")
+        print(coeff.eta)
+
+        print("siga")
+        print(coeff.sig_a)
+
         for k in range(0, mesh.ng):
             sys = high_order_assembly(mesh, coeff, last_iteration[:, :2*mesh.nx], k)
-            if k== 0:
-                numpy.set_printoptions(precision=2)
-                # print(sys.mat.todense())
-                # print("matrix")
+            # print(I_prev[k, 0:6] / (mesh.C*mesh.dt))
+            # print("S - q should be this")
 
-                # print(sys.src)
-                # print("source")
 
-                # print(sparse.linalg.inv(sys.mat) @ sys.src)
-                # print("Direct solution")
-    
-
-            updated_solution[k, :], b = sparse.linalg.lgmres(sys.mat, sys.src, atol=mesh.eps, rtol = mesh.eps, x0=last_iteration[k])
-        if b !=0:
-            print(f"GMRES error, {b}")
+            # updated_solution[k, :], b = sparse.linalg.lgmres(sys.mat, sys.src, atol=mesh.eps, rtol = mesh.eps, x0=last_iteration[k])
+            # updated_solution[k,:] = sparse.linalg.spsolve(sys.mat, sys.src)
+            updated_solution[k,:] = sparse.linalg.inv(sys.mat) @ sys.src
+            print(f"Solved group {k} directly")
+        # if b !=0:
+        #     print(f"GMRES error, {b}")
         
         # print(f"Call = {iter}")
 
@@ -217,11 +446,18 @@ def unaccelerated_loop(mesh : tools.Discretization,
         #     print("  Left values:", updated_solution[0, 2*mesh.nx:2*mesh.nx+6:2])
         #     print("  Right values:", updated_solution[0, 2*mesh.nx+1:2*mesh.nx+6:2])
 
-        
-        diff =  abs(updated_solution - last_iteration)
-        diff = diff / numpy.maximum(abs(last_iteration), 1e-10)
-        change = numpy.linalg.norm(diff, 2, axis=0)
         last_iteration[:] = updated_solution.copy()
+
+        diff = abs((updated_solution / last_iteration)- 1) 
+        
+        # diff =  abs(updated_solution - last_iteration)
+        # diff = diff / numpy.maximum(abs(last_iteration), 1e-2)
+        change = numpy.linalg.norm(diff, 2, axis=1)
+        print(change)
+        print("change vector")
+        
+
+        print()
 
     
         
@@ -254,18 +490,18 @@ def update_temperature(mesh : tools.Discretization,
                        Q = 0):
     # TODO
 
-    print(soln.cell_center_i[0])
-    print("intensity")
-    print(coeff.beta[0])
-    print("beta")
-    print(coeff.kappa[0] * (soln.cell_center_i[0] - coeff.beta[0]))
-    print("diff")
+    # print(soln.cell_center_i[0])
+    # print("intensity")
+    # print(coeff.beta[0])
+    # print("beta")
+    # print(coeff.kappa[0] * (soln.cell_center_i[0] - coeff.beta[0]))
+    # print("diff")
 
-    print((Cv/mesh.dt))
-    print("Cv/dt")
+    # print((Cv/mesh.dt))
+    # print("Cv/dt")
 
-    print(numpy.sum(coeff.kappa * coeff.db_dt, axis = 0))
-    print("kappa * dbdt")
+    # print(numpy.sum(coeff.kappa * coeff.db_dt, axis = 0))
+    # print("kappa * dbdt")
     temp = (
         numpy.sum(coeff.kappa * (soln.cell_center_i - coeff.beta), axis=0) + Q
     )/(

sample_input.py
@@ -2,14 +2,17 @@ import numpy
 import scipy.integrate as integrate
 import scipy.sparse as sparse
 import tools # type: ignore
-from physics import H, K, A_R, C# type: ignore
+from physics import A_R, C# type: ignore
 import method # type: ignore
 import physics # type: ignore
 import matplotlib
 import matplotlib.pyplot as plt
 from itertools import cycle
+import copy
 
 
+
+numpy.set_printoptions(precision=2)
 
 
 # Define temperature-dependent coefficients as functions
@@ -18,13 +21,22 @@ from itertools import cycle
 
 def FC_opacity(T, nu, k0):
     def num_func(n):
-        return (H**3)*(n**7)*(1-numpy.exp(-H*n/(K*T)))**-2
+        return (n**7)*(1-numpy.exp(-physics.H*n/(physics.K*T)))**-3
     def dem_func(n):
-        return (n**4)*(1-numpy.exp(-H*n/(K*T)))**-2
+        return (n**4)*(1-numpy.exp(-physics.H*n/(physics.K*T)))**-2
+    
+    
 
     numerator, err = integrate.quad(num_func, nu[0], nu[1])
     denominator, err = integrate.quad(dem_func, nu[0], nu[1])
-    return k0 * denominator/numerator
+
+    if numerator == 0:
+        print(T)
+        print(nu)
+        print(numerator)
+        print(denominator)
+
+    return (1000*3)*k0 * denominator/(numerator*((physics.H)**3))
 
 def group_FC_opacity(mesh, T, k0):
     kappa = numpy.zeros((mesh.ng, mesh.nx))
@@ -41,6 +53,17 @@ def FC_heatcap(T_b, mesh):
 # Units for scaling computations, such that
 # x_physical = x_working * scale_x
 
+
+
+# test_kappa = FC_opacity(1/physics.K, [2000, 2002]/physics.H, 27)
+# print(test_kappa)
+
+# plt.figure()
+# plt.show()
+
+
+
+
 scale = tools.Scales()
 mesh = tools.Discretization()
 
@@ -50,7 +73,7 @@ k_star = 27
 
 mesh.groups = numpy.array([0.000, 0.7075, 1.415, 2.123, 2.830, 3.538, 4.245,
     5.129, 6.014, 6.898, 7.783, 8.667, 9.551, 10.44, 11.32, 12.20, 
-    13.09, 1e4])*(1000/H)
+    13.09, 1e4])*(1000/physics.H)
 mesh.dx = 0.4
 
 mesh.I_BC = numpy.zeros((mesh.ng, 2))
@@ -61,13 +84,23 @@ mesh.F_BC = numpy.zeros((mesh.ng, 2))
 
 
 
-mesh.dt = 2e-3 * 1e-8 # seconds
+mesh.dt = 2e-1 * 1e-8 # seconds
 
-mesh.eps = 1e-16
-
-
+mesh.eps = 1e-14
 
 
+
+print(mesh.groups)
+print("groups, hz")
+
+print(mesh.groups * physics.H)
+print("groups, eV")
+
+
+
+
+# plt.figure()
+# plt.show()
 
 
 
@@ -80,77 +113,152 @@ mesh.eps = 1e-16
 
 
 T_prev = (1/physics.K).astype(numpy.float128)*numpy.ones((mesh.nx))
+T_bound = (1000/physics.K).astype(numpy.float128)*numpy.ones((mesh.nx))
 kappa = group_FC_opacity(mesh, T_prev, k_star)
 
-Cv    = FC_heatcap(1, mesh)
+
+Cv    = FC_heatcap(1/physics.K, mesh)
+
+
+print(Cv)
+print("Cv")
+print("NTOE Cv has been modified-- check back later")
 
 Q     = numpy.zeros((mesh.ng, mesh.nx))
 
-
-mesh.I_BC[:, 0] = (physics.group_planck(mesh, 1000*T_prev))[:, 0]
-mesh.F_BC[:, 0] = 0.5*(physics.group_planck(mesh, 1000*T_prev))[:, 0]
+mesh.I_BC[:, 0] = (physics.group_planck(mesh, T_bound))[:, 0]
+mesh.F_BC[:, 0] = 0.5*(physics.group_planck(mesh, T_bound))[:, 0]
 
 print(mesh.I_BC[:, 0])
 print("bound")
 
 # print(physics.group_planck(mesh, T_prev))
 
+kappa = group_FC_opacity(mesh, T_prev, k_star)
+coeff = tools.MG_coefficients(mesh)
 
 
 sol_prev = tools.Transport_solution(mesh)
-
 sol_prev.intensity[:,:] = tools.dbl(physics.group_planck(mesh, T_prev))
-# sol_prev.intensity[0:2,0:4] = 3*tools.dbl(physics.group_planck(mesh, T_prev))[0:2,0:4]
+print(sol_prev.intensity[:,0:6])
 
-# print(sol_prev.vec[0])
-# print("initial")
-
-# print("initial condition")
-
-
-coeff = tools.MG_coefficients(mesh)
 coeff.assign(mesh, kappa, sol_prev, T_prev, Cv, Q)
 
+# print(kappa[:,0])
+# print("kappa original")
+# print(coeff.kappa[:,0])
+# print("coeff kappa")
+# print(coeff.D[:, 0])
+# print("D, coeff")
 
 
+plt.figure()
+ax = plt.gca()
+lines = tools.LD_plottable(mesh, sol_prev.vec)
+tools.plot_LD_groups(mesh, lines.intensity, [0])
+plt.autoscale()
+
+
+# plt.show()
+
+
+# plt.figure()
+
+change = T_prev.copy()
 b = tools.Transport_solution(mesh)
-b.vec[:,:]  = method.unaccelerated_loop(mesh, sol_prev, T_prev, kappa, Cv, Q)
 
-print(Cv)
-print("Cv")
+solutions = []
+solutions.append(copy.deepcopy(b))
 
-
-test = method.update_temperature(mesh, coeff, b, Cv)
-print(test)
-print("temp update")
-
-
-T_prev = T_prev + test
-
-# print(T_prev)
-# print("new temp")
-
-newcoeff = tools.MG_coefficients(mesh)
-newcoeff.assign(mesh, kappa, b, T_prev, Cv, Q)
-
-kappa = group_FC_opacity(mesh, T_prev, k_star)
-
-c = tools.Transport_solution(mesh)
-c.vec[:,:] = method.unaccelerated_loop(mesh, b, T_prev, kappa, Cv, Q)
+# plt.plot(mesh.cell_centers, T_prev*physics.K, label=f"{0:.2e}")
+# plt.title("Temperature, initial")
 
 
 
 
+temp_vec = []
+
+sol_test = tools.Transport_solution(mesh)
+
+plt.figure()
+ax = plt.gca()
+lines = tools.LD_plottable(mesh, sol_prev.vec)
+tools.plot_LD_groups(mesh, lines.intensity, range(0, mesh.ng))
+plt.title("previous solution, kappa=1")
+plt.autoscale()
+
+
+kappa_test = 100*numpy.ones((mesh.nx))
+
+coeff = tools.MG_coefficients(mesh)
+coeff.assign(mesh, kappa_test, sol_prev, T_prev, Cv, Q)
+
+
+mat_test = method.global_mat_elementwise(mesh, kappa_test, 1/(3*kappa_test))
+src_test = method.get_HO_source(mesh, sol_prev.intensity, coeff, 0)
+
+sol_test.vec[:] = sparse.linalg.inv(mat_test) @ src_test
+
+plt.figure()
+ax = plt.gca()
+lines = tools.LD_plottable(mesh, sol_test.vec)
+tools.plot_LD_groups(mesh, lines.intensity, [0])
+plt.title("Test intensity, kappa=1")
+plt.autoscale()
+
+
+plt.figure()
+ax = plt.gca()
+lines = tools.LD_plottable(mesh, sol_test.vec)
+tools.plot_LD_groups(mesh, lines.flux, range(0, mesh.ng))
+plt.title("Test flux, kappa=1")
+plt.autoscale()
 
 
 
+plt.show()
 
+
+
+for t in range(0, 20):
+    # kappa[:] = group_FC_opacity(mesh, T_prev, k_star)
+    kappa[:] = numpy.tile(27/(mesh.groups[1:]*physics.H/1000)**3, reps = (mesh.nx, 1)).transpose()
+    
+
+    b.vec[:,:]  = method.unaccelerated_loop(mesh, sol_prev, T_prev, kappa, Cv, Q)
+
+    solutions.append(copy.deepcopy(b))
+
+    sol_prev.vec[:] = b.vec[:].copy()
+    coeff = tools.MG_coefficients(mesh)
+    coeff.assign(mesh, kappa, b, T_prev, Cv, Q)
+
+    print(coeff.kappa.shape)
+    print("kappa shape")
+    print(coeff.kappa[:, 0])
+    print("kappa")
+
+
+    change[:] = method.update_temperature(mesh, coeff, b, Cv)
+    T_prev = T_prev + change[:].copy()
+    coeff.assign(mesh, kappa, b, T_prev, Cv, Q)
+    temp_vec.append(T_prev)
+
+plt.figure()
+
+for t in range(0, len(temp_vec)):
+    plt.plot(mesh.cell_centers, temp_vec[t]*physics.K, label=f"{(t+1)*mesh.dt:.2e}")
+plt.legend()
+plt.title("Temperature, final")
+
+    
 plt.figure()
 ax = plt.gca()
 lines = tools.LD_plottable(mesh, b.vec)
 tools.plot_LD_groups(mesh, lines.intensity, range(0, mesh.ng))
 plt.title("b, intensity")
 ax.autoscale()
+
 
 
 plt.figure()
@@ -160,23 +268,23 @@ tools.plot_LD_groups(mesh, lines.flux, range(0, mesh.ng))
 plt.title("b, flux")
 ax.autoscale()
 
+
+plt.figure()
+ax = plt.gca()
+
+for i in range(0, len(solutions)):
+    lines = tools.LD_plottable(mesh, solutions[i].vec)
+    tools.plot_LD_groups(mesh, lines.intensity, [0])
+plt.title("intensity over time")
+ax.autoscale()
+
+
+
+
 plt.show()
 
+print(coeff.kappa[:])
+print("kappa")
 
-
-def planck(T, nu):
-    B = 8*numpy.pi*(H*nu)**3/((C**2 * H**2)*(numpy.exp((H*nu)/(K*T)) - 1))
-    return(B)
-
-c2 = tools.MG_coefficients(mesh)
-c2.assign(mesh, kappa, sol_prev, T_prev*1000, Cv, 0)
-
-# plt.figure()
-# plt.plot(H*nu, planck(T_prev[0]*1000, nu))
-# plt.stairs(c2.beta[:, 0]/numpy.diff(mesh.groups), edges = H*mesh.groups)
-# plt.xscale('log')
-# plt.yscale('log')
-# plt.ylim(1e-8, 1e20)
-# plt.show()
 
 